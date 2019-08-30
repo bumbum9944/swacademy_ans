641f1d6c6d5d732e94172639ddec1abaa2b3b88b
bumbum94 <tong940526@gmail.com>
2019-08-30 16:14:21 +0900

    자리이동 백업

--- p_1961.py
+++ p_1961.py
@@ -6,10 +6,13 @@ for test in range(1, tests+1):
         matrix.append(list(map(int, input().split())))
 
 
-    def rotation(matrix):
-        matrix_col90 = list(zip(*matrix))
-        matrix_col180 = list(zip(*matrix))
-        matrix_col270 = list(zip(*matrix))
+def rotation(matrix):
+    matrix_col90 = list(zip(*matrix))
+    matrix_col180 = list(zip(*matrix))
+    matrix_col270 = list(zip(*matrix))
 
-        for i in range(N):
-            for i 
+        
+def prin(matrix_90, matrix_180, matrix_270):
+    N = len(rot_matrix)
+    for i in range(N):
+        m90 = 
